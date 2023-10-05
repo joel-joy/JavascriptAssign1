624e66a092ffcd91b549349485ef7e6cce1c250e
Joel Parappuram Joy <122415887+joel-joy@users.noreply.github.com>
2023-10-04 23:29:22 -0400

    Update storymaker.js

--- storymaker.js
+++ storymaker.js
@@ -7,7 +7,7 @@ var noun1But = document.getElementById("noun1");
 var verbBut = document.getElementById("verb");
 var adjectiveBut = document.getElementById("adjective");
 var noun2But = document.getElementById("noun2");
-var settingBut = document.getElementById("setting");
+var settingBut = document.getElementById('setting');
 
 // Constants for p tag to display query selectors
 var choosenNoun1Element = document.getElementById("choosenNoun1");
@@ -65,7 +65,7 @@ function setting_on_click() {
 
 // concatenate the user story and display
 function playback_on_click() {
-    var story = `${choosenNoun1} ${choosenVerb} ${choosenAdjective} ${choosenNoun2} ${choosenSetting}.`;
+    var story = `${choosenNoun1} ${choosenVerb} ${choosenAdjective} ${choosenNoun2} in ${choosenSetting}.`;
     storyElement.textContent = story;
 }
 
@@ -77,24 +77,26 @@ function random_on_click() {
     choosenNoun2 = getRandomWord(noun2);
     choosenSetting = getRandomWord(settings);
 
-    var story = "${choosenNoun1} ${choosenVerb} ${choosenAdjective} ${choosenNoun2} ${choosenSetting}.";
+    var story = `${choosenNoun1} ${choosenVerb} ${choosenAdjective} ${choosenNoun2} in ${choosenSetting}.`;
     storyElement.textContent = story;
 }
 
 /* Event Listeners
 -------------------------------------------------- */
-noun1But.addEventListener("click", noun1_on_click);
-verbBut.addEventListener("click", verb_on_click);
-adjectiveBut.addEventListener("click", adjective_on_click);
-noun2But.addEventListener("click", noun2_on_click);
-settingBut.addEventListener("click", setting_on_click);
+noun1But.addEventListener('click', noun1_on_click);
+verbBut.addEventListener('click', verb_on_click);
+adjectiveBut.addEventListener('click', adjective_on_click);
+noun2But.addEventListener('click', noun2_on_click);
+settingBut.addEventListener('click', setting_on_click);
 
-playbackBut.addEventListener("click", playback_on_click);
-randomBut.addEventListener("click", random_on_click);
+playbackBut.addEventListener('click', playback_on_click);
+randomBut.addEventListener('click', random_on_click);
 
+// Function to add student ID/name dynamically
 document.getElementById("200535886").textContent = "Joel Parappuram Joy";
 
+// Function to get a random word from an array
 function getRandomWord(array) {
-    var randomIndex = Math.floor(Math.random()*array.length);
+    var randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 }
